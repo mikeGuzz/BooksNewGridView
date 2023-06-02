1b3fb26ba6cba316d2d80963563e7b46e2d3f520
mikeGuzz <larrygunior@gmail.com>
2023-06-03 02:39:12 +0300

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.exec_button = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 82;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(1136, 647);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // exec_button
+            // 
+            this.exec_button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.exec_button.Location = new System.Drawing.Point(961, 665);
+            this.exec_button.Name = "exec_button";
+            this.exec_button.Size = new System.Drawing.Size(187, 41);
+            this.exec_button.TabIndex = 1;
+            this.exec_button.Text = "Execute (async)";
+            this.exec_button.UseVisualStyleBackColor = true;
+            this.exec_button.Click += new System.EventHandler(this.exec_button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1160, 718);
+            this.Controls.Add(this.exec_button);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button exec_button;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection conn = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async Task LoadTable()
+        {
+            if (conn == null)
+                return;
+
+            exec_button.Enabled = false;
+            DataTable table = new DataTable();
+
+            try
+            {
+                await conn.OpenAsync();
+
+                SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "SELECT N, Code, New, Name, Price, Pages, Date, Pressrun, " +
+                    "[Spr_format].Format, [Spr_izd].Izd, [Spr_kategory].Category, [Spr_themes].Themes " +
+                    "FROM [dbo].books_new " +
+                    "INNER JOIN Spr_format ON format_id = [Spr_format].Id " +
+                    "INNER JOIN Spr_izd ON izd_id = [Spr_izd].Id " +
+                    "INNER JOIN Spr_kategory ON kategory_id = [Spr_kategory].Id " +
+                    "INNER JOIN Spr_themes ON themes_id = [Spr_themes].Id";
+
+                using (SqlDataReader rdr = await cmd.ExecuteReaderAsync())
+                {
+                    for (int i = 0; i < rdr.VisibleFieldCount; i++)
+                    {
+                        table.Columns.Add(rdr.GetName(i));
+                    }
+                    while (await rdr.ReadAsync())
+                    {
+                        DataRow row = table.NewRow();
+                        for (int i = 0; i < rdr.VisibleFieldCount; i++)
+                        {
+                            row[i] = await rdr.GetFieldValueAsync<object>(i);
+                        }
+                        table.Rows.Add(row);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+
+                dataGridView1.DataSource = table;
+                exec_button.Enabled = true;
+            }
+        }
+
+        private async void exec_button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string connString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+                conn = new SqlConnection(connString);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            
+            await LoadTable();
+        }
+    }
+}
